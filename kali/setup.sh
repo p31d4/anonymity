968a8dc3e5c206e6d5c6fac1bdee090a8cd33f83
p31d4 <166958893+p31d4@users.noreply.github.com>
2024-05-22 19:39:11 +0000

    first script for live Kali (2024.01)

---
+++ kali/setup.sh
@@ -0,0 +1,57 @@
+#!/bin/zsh
+
+if [ `id -u` -ne 0 ]
+then 
+    echo "Please run as root!"
+    exit 1
+fi
+
+# From apt-cache policy:
+# - docker.io=20.10.25+dfsg1-3
+packages=(
+	keepassxc
+	tor
+	weechat
+	openresolv
+	docker.io
+)
+
+passwd kali
+
+pushd /etc/sudoers.d
+rm kali-grant-root live
+popd
+
+# Change MAC Address
+# From Stealing the Network :)
+#ifconfig wlan0 hw ether AA:BB:DD:EE:55:11
+ip link set dev wlan0 address AA:BB:DD:EE:55:11
+
+
+# Disable ipv6 - ProtonVPN does not support it
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+
+# Connect to Wi-Fi
+#nmcli dev wifi connect $1 password $2 hidden yes
+nmcli c add type wifi con-name "kali_tmp" ifname wlan0 ssid $1
+nmcli con modify "kali_tmp" wifi-sec.key-mgmt wpa-psk
+nmcli con modify "kali_tmp" wifi-sec.psk $2
+nmcli con up "kali_tmp"
+
+apt update
+apt install $packages -y
+
+# ProtonVPN
+wget "https://raw.githubusercontent.com/ProtonVPN/scripts/master/update-resolv-conf.sh" -O "/etc/openvpn/update-resolv-conf"
+chmod +x "/etc/openvpn/update-resolv-conf"
+
+# TODO: git config file
+
+# Connect to the VPN
+# Go to https://account.protonvpn.com/account and get username/password
+
+# TODO: Configure Firefox:
+# - remove every search engine but DuckGo
+# - import burpsuite certificate and enable proxy to 127.0.0.1
